2a59f5e189cd4e5848363a6b0e24b73cca10bbb1
Sam Rowell - Mac05F <S.Rowell@2010.ljmu.ac.uk>
2012-10-23 13:55:23 +0100

    Found the files!

---
+++ my_game.rb
@@ -0,0 +1,21 @@
+player_1 = "Aaron"
+player_1_health = 60
+player_2 = "Molvak"
+player_1_health = 70
+player_3 = "Syntha"
+player_1_health = 80
+
+puts "Players:"
+puts "\t #{player_1}"
+puts "\t #{player_2}"
+puts "\t #{player_3}"
+
+puts "#{player_1.capitalize}'s health is #{player_1_health}" #{Player_1_Health*5} for *5
+puts "#{player_2.uppercase}'s health is #{player_2_health}" 
+
+
+puts player_1_health/9.00 #for a decimal output
+puts player_1_health/9	  #for a integer output
+
+
+
---
+++ my_game_old.rb
@@ -0,0 +1,21 @@
+greeting = "Welcome!"
+puts greeting
+
+3.times do
+  puts greeting.upcase
+end
+
+puts "Number of hours in a year: "
+year = 1*365*24 
+puts year 
+
+message = "Hello, Internet & Open Source Programming!" 
+puts message.upcase
+
+puts Time.new
+
+puts "Aarons'health is 60"
+puts "Players"
+puts "/t Aaron"
+puts "/t Molvak"
+puts "/t Syntha"
