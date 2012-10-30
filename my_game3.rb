463f3d957f332280af09ecddb7184ebd46670a19
Sam Rowell - Mac05F <S.Rowell@2010.ljmu.ac.uk>
2012-10-30 13:58:49 +0000

    completed activity 3

--- my_game3.rb
+++ my_game3.rb
@@ -1,11 +1,16 @@
 class Player
+  attr_reader :health
+  attr_accessor :name
+    #attr_writer
+
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
+    @score = score
 	end
 
 	def to_s
-  	"I'm #{@name} with a health of #{@health}."
+  	"I'm #{@name} with a health of #{@health} and a score of #{@score}."
   end
 
   def blam
@@ -14,6 +19,10 @@ class Player
 
   def w00t
   	@health +=10
+  end
+
+  def score
+    @health * @name.length
   end
 end
 
@@ -32,8 +41,22 @@ puts player_3
 player_4 = Player.new("lorek",90)
 #puts player_4.inspect
 puts player_4
+
 player_4.blam
 puts player_4
+puts ""
+puts player_1.health
+puts player_2.health
+puts player_3.health
+puts player_4.health
+
+puts player_2.name = "murda"
+puts player_2.name
+
+puts player_2
+puts 
+
+
 
 
 
